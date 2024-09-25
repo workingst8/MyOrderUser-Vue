7d0712c3cf05aa3be5f8dc2eb8e8bcd68c56dada
workingst8 <workingst8@gmail.com>
2024-09-25 18:01:33 +0900

    Feat: Google Analytics 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
-import { createApp } from 'vue'
-import './style.scss'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import './style.scss';
+import App from './App.vue';
+import router from './router';
+import VueGtag from 'vue-gtag-next';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+
+app.use(VueGtag, {
+  property: {
+    id: 'G-49GLM0FNPS'
+  }
+});
+
+app.mount('#app');
